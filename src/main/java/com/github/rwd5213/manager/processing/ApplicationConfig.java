5ea0fcc32cdd6e001f96932d7f27ff894ee001ba
Ryan Despres <Ryan Despres>
2017-05-06 17:19:55 +0100

    Added application config

--- src/main/java/com/github/rwd5213/manager/processing/ApplicationConfig.java
+++ src/main/java/com/github/rwd5213/manager/processing/ApplicationConfig.java
@@ -1,5 +1,14 @@
 package com.github.rwd5213.manager.processing;
 
-public class ApplicationConfig {
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
 
+import com.github.rwd5213.manager.resource.SubmitResource;
+
+@ApplicationPath("/manager")
+public class ApplicationConfig extends ResourceConfig {
+	
+	public ApplicationConfig(){
+		register(SubmitResource.class);
+	}
 }
